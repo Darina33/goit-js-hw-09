a5db674f9d9d37c41ff809ae645ceae2eb84c1f8
Darina33 <115955389+Darina33@users.noreply.github.com>
2023-04-08 16:54:19 +0300

    add promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  let firstDelay = e.currentTarget.delay.valueAsNumber;
+  const step = e.currentTarget.step.valueAsNumber;
+  const amount = e.currentTarget.amount.valueAsNumber;;
+
+  for (i = 1; i <= amount; i++) {
+    const position = i;
+    createPromise(position, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    firstDelay += step;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
 }
